c17d619b28517c012b46a77c77b265423b9fa4a8
piaacv <122986202+piaacv@users.noreply.github.com>
2023-07-26 02:20:42 -0400

    options

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // };
 const fs = require("fs");
 const path = require("path");
+const fetch = require("node-fetch");
 // to have path
 const route = process.argv[2];
 console.log(route, "input route");
@@ -44,21 +45,94 @@ console.log(`Should be a .md file not ${extention}`);
   // console.log(fileContent);
   
   // is there links?
-  const searchLinks = fileContent
-  const resultSearchLinks = /\[(.+)\]\(([^ ]+?)( "(.+)")?\)/.test(searchLinks);
-  function checkLinks(){
-    let findingLinks;
+  const resultSearchLinks = /\[(.+)\]\(([^ ]+?)( "(.+)")?\)/.test(fileContent);
+  let findingLinks;
     if(resultSearchLinks){
       findingLinks = 'It has links';
     } else {
       findingLinks = 'It has no links';
     }
-    console.log(`Does the file have links? ${findingLinks}`);
+  console.log(`Does the file have links? ${findingLinks}`);
+  //Extract links
+  function extractLinks() {
+    const searchingLinks = /\[(.+)\]\(([^ ]+?)( "(.+)")?\)/g
+    const linksFound = [];
+    let match;
+    while((match = searchingLinks.exec(fileContent)) !== null) {
+     const text = match[1];
+     const url = match[2];
+     linksFound.push({text,url});
+    };
+    return linksFound;
   }
+  const linksExtractedFound = extractLinks(fileContent);
+  console.log(`The fail has the following links:`)
+  linksExtractedFound.forEach((link,index) => {
+    console.log(`Link ${index + 1}:`);
+    console.log(`Text: ${link.text}`);
+    console.log(`URL: ${link.url}`);
+  })
+  //Function validateLink
+const validateLink = (link) => {
+  if (link && link.url){
+  return fetch(link.url)
+  .then((response) =>{
+    if(response.status >= 200 && response.status < 400) {
+      link.status = response.status;
+      link.ok = "Ok";
+      return link;
+    } else {
+      link.status = response.status;
+      link.ok = "Broke";
+      return link;
+    }
+
+  })
+  .catch((error) => {
+    link.status = "Error";
+    link.ok = "Failed";
+    return link;
+})
+  }}
+  const validatePromises = linksExtractedFound.map((link) => validateLink(link));
+  Promise.all(validatePromises)
+  .then((validatedLinks) => {
+    // Imprimir los resultados de validación
+    validatedLinks.forEach((validatedLink, index) => {
+      console.log(`Link ${index + 1}:`);
+      console.log(`Text: ${validatedLink.text}`);
+      console.log(`URL: ${validatedLink.url}`);
+      console.log(`Status: ${validatedLink.status}`);
+      console.log(`OK: ${validatedLink.ok}`);
+    });
+
+    // Calcular y mostrar el total de links usando la función stats
+    console.log("stats:", stats(validatedLinks));
+    console.log("stats and validate:", statsAndValidate(validatedLinks));
+  })
+  .catch((error) => {
+    console.error("Error occurred during validation:", error);
+  });
   
-  //search for url links
-  checkLinks();
+//Function stats
+const stats =(links) =>{
+  return {
+    Total: links.length,
+    Unique: new Set(links.map((link) => link.url)).size,
+  }
+};
+
+}
+ //Function Stats and Validate
+const statsAndValidate = (links) => {
+  const brokenLinks = links.filter((link) => link.ok !=="Ok").length;
+  return {
+    Total: links.length,
+    Unique: new Set(links.map((link) => link.url)).size,
+    Broken: brokenLinks,
+  }
 }
 
-// the return value is a promise
 };
+
+
